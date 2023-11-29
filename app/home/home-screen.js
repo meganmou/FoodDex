9f3a0b23608f8c2e0470dfc4ec549c744e4412a9
Maya <maya.msc.chandra@gmail.com>
2023-11-29 09:54:00 -0800

    Co-authored-by: megan-tarjimly <megan-tarjimly@users.noreply.github.com>

--- app/home/home-screen.js
+++ app/home/home-screen.js
@@ -1,46 +1,130 @@
-import { StyleSheet, Text, View, Pressable, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { useState, useEffect } from "react";
-import { useRouter } from "expo-router";
+import { useNavigation } from "expo-router";
 import supabase from "../../Supabase";
 import { palette } from "../../assets/palette";
-
+import { Searchbar } from "react-native-paper";
+import TrendingRecipeComponent from "../../components/TrendingRecipeComponent";
+import NewCuisineComponent from "../../components/NewCuisineComponent";
 import { Link } from "expo-router";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 export default function Page() {
-  const [data, setData] = useState(null);
-  const router = useRouter();
+  // hide header bar
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
 
-  /*useEffect(() => {
-    // Fetch data on initial load
-    const fetchData = async () => {
+  // access trending recipe data
+  [recipeData, setRecipeData] = useState(null);
+  useEffect(() => {
+    const fetchRecipeData = async () => {
       const response = await supabase.from("Recipes").select("*");
-      setData(response.data);
+      setRecipeData(response.data);
     };
-    fetchData();
+    fetchRecipeData();
   }, []);
+  trending_recipes = [];
+  if (recipeData) {
+    trending_recipes.push(recipeData[0]);
+    trending_recipes.push(recipeData[8]);
+    trending_recipes.push(recipeData[13]);
+  }
+  //console.log("trending recipes");
+  //console.log(trending_recipes);
 
-  console.log(data[0].name);*/
+  // access new recipe data
+  [cuisineData, setCuisineData] = useState(null);
+  useEffect(() => {
+    const fetchCuisineData = async () => {
+      const response = await supabase.from("Cuisine Flags").select("*");
+      setCuisineData(response.data);
+    };
+    fetchCuisineData();
+  }, []);
+  new_cuisines = [];
+  if (cuisineData) {
+    new_cuisines.push(cuisineData[0]);
+    new_cuisines.push(cuisineData[1]);
+    new_cuisines.push(cuisineData[2]);
+  }
+  // console.log("new cuisines");
+  // console.log(new_cuisines);
+
+  // search bar functionality
+  const [searchQuery, setSearchQuery] = useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>FoodDex</Text>
-        <Link
-          href={{
-            pathname: "/home/my-profile-screen",
-          }}
-          asChild
-        >
+        <Text style={styles.titleText}>FoodDex</Text>
+        <View style={styles.topButtons}>
           <Pressable>
-            <Image
-              source={require("../../assets/my-profile-pic.png")}
-              style={styles.myProfilePic}
-            />
+            <View style={styles.friendsLink}>
+              <Text style={styles.friendsText}>4</Text>
+              <Text style={styles.friendsText}>friends</Text>
+            </View>
           </Pressable>
-        </Link>
+          <Link
+            href={{
+              pathname: "/home/my-profile-screen",
+            }}
+            asChild
+          >
+            <Pressable>
+              <Image
+                source={require("../../assets/my-profile-pic.png")}
+                style={styles.myProfilePic}
+              />
+            </Pressable>
+          </Link>
+        </View>
       </View>
-      <View style={styles.trending}></View>
-      <View style={styles.new}></View>
+      <View style={styles.searchBar}>
+        <Searchbar
+          placeholder="Search for a cuisine, recipe, etc..."
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </View>
+      <View style={styles.trending}>
+        <Text style={styles.subtitleText}>Trending Recipes</Text>
+        <FlatList
+          data={trending_recipes}
+          renderItem={({ item }) => (
+            <TrendingRecipeComponent
+              name={item.name}
+              cuisine={item.cuisine}
+              photo={item.photo}
+            />
+          )}
+          keyExtractor={(item) => item.name}
+          horizontal
+        />
+      </View>
+      <View style={styles.new}>
+        <Text style={styles.subtitleText}>New Cuisines for You</Text>
+        <FlatList
+          data={new_cuisines}
+          renderItem={({ item }) => (
+            <NewCuisineComponent country={item.country} flag={item.flag} />
+          )}
+          keyExtractor={(item) => item.country}
+          horizontal
+        />
+      </View>
     </View>
   );
 }
@@ -49,26 +133,58 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    padding: 24,
     backgroundColor: palette.lightGray,
-  },
-  title: {
-    fontFamily: "DM Serif Display Regular",
-    fontSize: 40,
-    color: palette.lightGreen,
+    paddingTop: 50,
   },
   header: {
     justifyContent: "space-between",
     width: "100%",
     flexDirection: "row",
     alignItems: "center",
+    borderBottomColor: "gray",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    padding: 10,
   },
-  trending: {},
-  new: {},
+  titleText: {
+    fontFamily: "DM Serif Display Regular",
+    fontSize: 40,
+    color: palette.lightGreen,
+  },
+  topButtons: {
+    flexDirection: "row",
+  },
+  friendsLink: {
+    alignItems: "center",
+  },
+  friendsText: {
+    fontFamily: "Nunito Regular",
+    fontSize: 17,
+    paddingRight: 5,
+  },
   myProfilePic: {
     resizeMode: "contain",
     height: 40,
     width: 40,
     borderRadius: 20,
+  },
+  searchBar: {
+    width: "100%",
+    padding: 10,
+  },
+  trending: {
+    // flex: 1,
+    height: windowHeight * 0.4,
+    width: "100%",
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  subtitleText: {
+    fontFamily: "DM Serif Display Regular",
+    fontSize: 27,
+  },
+  new: {
+    width: "100%",
+    padding: 10,
   },
 });
---
+++ components/TrendingRecipeComponent.js
@@ -0,0 +1,81 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Pressable,
+  Dimensions,
+} from "react-native";
+import { Link, useLocalSearchParams } from "expo-router";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const TrendingRecipe = ({ name, cuisine, photo }) => {
+  return (
+    <Link
+      href={{
+        pathname: "home/trendingRecipe",
+        params: {
+          recipeName: name,
+        },
+      }}
+      asChild
+    >
+      <Pressable>
+        <View style={styles.recipeContainer}>
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: photo }} style={styles.recipePhoto} />
+          </View>
+          <View style={styles.recipeInfo}>
+            <Text style={styles.nameText}>{name}</Text>
+            <Text style={styles.cuisineText}>{cuisine}</Text>
+          </View>
+        </View>
+      </Pressable>
+    </Link>
+  );
+};
+
+export default TrendingRecipe;
+
+const styles = StyleSheet.create({
+  recipeContainer: {
+    //do windowWidth
+    width: windowWidth * 0.8,
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+    padding: 5,
+    //borderColor: "red",
+    //borderWidth: 2,
+  },
+  recipeInfo: {
+    //flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    //backgroundColor: "orange",
+    marginTop: 5,
+  },
+  nameText: {
+    fontFamily: "Nunito Bold",
+    fontSize: 20,
+    paddingRight: 10,
+  },
+  cuisineText: {
+    fontFamily: "Nunito Regular",
+    fontSize: 15,
+  },
+  imageContainer: {
+    width: "100%",
+    height: windowHeight * 0.25,
+    // paddingLeft: 5,
+    //backgroundColor: "green",
+    justifyContent: "flex-start",
+  },
+  recipePhoto: {
+    // resizeMode: "contain",
+    height: "100%",
+    width: "100%",
+    borderRadius: 10,
+  },
+});
